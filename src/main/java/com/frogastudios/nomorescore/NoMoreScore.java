11fa6159dc55c6105b1fae4fbca5e44e973bc92e
ExoPlant <lennyj2468+github@gmail.com>
2021-08-20 23:41:59 +0100

    Init

---
+++ src/main/java/com/frogastudios/nomorescore/NoMoreScore.java
@@ -0,0 +1,11 @@
+package com.frogastudios.nomorescore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NoMoreScore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
